2669b340f9ad4d27835577965bc835d11b320f3d
Francis Dias <francisdiasbr@gmail.com>
2024-07-29 23:31:21 -0300

    Add OpenAI route (/suggestion)

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from ratings.routes import ratings_bp
+from suggestion.routes import suggestion_bp
 import config
 
 # cria uma instância do Flask
@@ -8,6 +9,7 @@ app = Flask(__name__)
 
 # registra o blueprint das rotas
 app.register_blueprint(ratings_bp)
+app.register_blueprint(suggestion_bp)
 
 # função principal para iniciar o servidor
 if __name__ == "__main__":
---
+++ api/suggestion/routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+from suggestion.controller import search_movie_suggestion
+
+suggestion_bp = Blueprint("suggestion", __name__)
+
+@suggestion_bp.route("/suggestion", methods=["GET"])
+def get_suggestions():
+    return search_movie_suggestion()
